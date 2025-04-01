9bd6561fecc8b1d31aed2393a4e0b57f13e2091e
Diego-ferrreira <df202536@gmail.com>
2025-03-31 22:45:03 -0300

    map dos feeds

--- frontend/vite-project/src/components/Feeds/Feeds.jsx
+++ frontend/vite-project/src/components/Feeds/Feeds.jsx
@@ -1,1 +1,16 @@
-// 13214
+import React from 'react'
+import './Feeds.css'
+
+import Feed from './Feed'
+
+import HomeFeedData from '../../FakeApis/HomeFeedData'
+
+export default function Feeds() {
+  return (
+    <div className='feeds'>
+        {HomeFeedData.map (feed => (
+            <Feed feed={feed} key={feed.key} />
+        ))}
+    </div>
+  )
+}
