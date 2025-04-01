0074a4f7ccd7a078d72849965e6379898ab88e75
matheus915 <mstheusferreira12@gmail.com>
2025-03-31 22:31:56 -0300

    Tela do modo dark

--- frontend/vite-project/src/components/DarkMod/DarkMod.jsx
+++ frontend/vite-project/src/components/DarkMod/DarkMod.jsx
@@ -1,1 +1,17 @@
-// 12143
+import React from 'react'
+import './DarkMod.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
+
+export default function DarkMod() {
+    const DarkHandle = () => {
+        document.querySelector ('body').classList.toggle ('darkmood')
+    }
+
+  return (
+    <div className='dark-mood-icon'>
+        <FontAwesomeIcon icon={faLightbulb} onClick={DarkHandle} />
+    </div>
+  )
+}
