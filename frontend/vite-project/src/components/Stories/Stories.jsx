73185243d75de384056518c52a14c7d73c3cbd2c
David Khauan <thalsmatsumira@gmail.com>
2025-03-31 22:56:07 -0300

    componente stories

--- frontend/vite-project/src/components/Stories/Stories.jsx
+++ frontend/vite-project/src/components/Stories/Stories.jsx
@@ -1,1 +1,35 @@
-// wewt
+import React from 'react'
+import './Stories.css'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+
+import StoriesData from '../../FakeApis/StoriesData'
+
+import UserStory from './UserStory'
+
+export default function Stories() {
+  return (
+    <div className='stories'>
+        <UserStory />
+
+        <Swiper style={{ width: '80%' }}
+            slidesPerView={4}
+            spaceBetween={10}
+        >
+            {StoriesData.map ((story) => (
+                <SwiperSlide key={story.id}>
+                    <div className="story">
+                        <div className="user">
+                            <img src={story.storyProfile} alt="Perfil do Usuário" />
+                        </div>
+                        <img src={story.story} alt="Stories" />
+
+                        <h5>{story.name}</h5>
+                    </div>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    </div>
+  )
+}
--- frontend/vite-project/src/components/Stories/UserStory.jsx
+++ frontend/vite-project/src/components/Stories/UserStory.jsx
@@ -1,1 +1,30 @@
-// wewt
+import React from 'react'
+import './Stories.css'
+
+import CurrentUserData from '../../FakeApis/CurrentUserData'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAdd } from '@fortawesome/free-solid-svg-icons'
+
+export default function UserStory() {
+  return (
+    <div className='story userStory'>
+        <div className="user">
+            <img 
+                src={CurrentUserData.map (user => (user.ProfileImage))}
+                alt="Perfil do Usuário" 
+            />
+        </div>
+
+            <img 
+                src={CurrentUserData.map (user => (user.CoverPhoto))}
+                alt="Stories" 
+            />
+            <label htmlFor="storyFiles">
+                <FontAwesomeIcon icon={faAdd} />
+                <input type="file" id='storyFiles' />
+            </label>
+            <h5>Adicionar ao Story</h5>
+    </div>
+  )
+}
